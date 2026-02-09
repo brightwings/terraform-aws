bf4f10d7e95c9bcda51e36c9895f885a33996d18
Isaac Musselwhite <isaac@steadycloud.io>
2026-02-09 13:24:56 -0600

    Add tf-user-nicole and assign administrators group
    - Create new IAM user tf-user-nicole
    - Add isaac to administrators group
    - Add nicole to administrators group

--- main.tf
+++ main.tf
@@ -39,6 +39,15 @@ resource "aws_iam_user" "nicole" {
   }
 }
 
+resource "aws_iam_user" "tf_user_nicole" {
+  name = "tf-user-nicole"
+  path = "/"
+
+  tags = {
+    ManagedBy = "terraform"
+  }
+}
+
 # AWS IAM Groups #
 resource "aws_iam_group" "administrators" {
   name = "administrators"
@@ -54,4 +63,19 @@ resource "aws_iam_group" "developers" {
 resource "aws_iam_group_policy_attachment" "administrators_admin_access" {
   group      = aws_iam_group.administrators.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# AWS IAM User Group Memberships #
+resource "aws_iam_user_group_membership" "isaac_groups" {
+  user = aws_iam_user.isaac.name
+  groups = [
+    aws_iam_group.administrators.name,
+  ]
+}
+
+resource "aws_iam_user_group_membership" "nicole_groups" {
+  user = aws_iam_user.nicole.name
+  groups = [
+    aws_iam_group.administrators.name,
+  ]
 }
